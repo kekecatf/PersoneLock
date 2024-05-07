6a6be9e3f12a3b95cfb9e3b6270002c83b52199a
Atif Kekeç <kekecatf@outlook.com>
2024-05-07 13:52:16 +0300

    day7

--- app/src/main/java/com/example/muhendislikprojesi/LoginPanel.kt
+++ app/src/main/java/com/example/muhendislikprojesi/LoginPanel.kt
@@ -1,5 +1,6 @@
 package com.example.muhendislikprojesi
 
+import android.annotation.SuppressLint
 import android.util.Log
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.Arrangement
@@ -11,6 +12,9 @@ import androidx.compose.foundation.text.KeyboardOptions
 import androidx.compose.material3.Button
 import androidx.compose.material3.ButtonDefaults
 import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.SnackbarHost
+import androidx.compose.material3.SnackbarHostState
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.material3.TextField
@@ -19,6 +23,7 @@ import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
+import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.focus.FocusDirection
@@ -35,6 +40,7 @@ import androidx.navigation.NavController
 import com.example.muhendislikprojesi.Retrofit.ApiUtils
 import com.example.muhendislikprojesi.Retrofit.Veriler
 import com.example.muhendislikprojesi.ui.theme.MuhendislikProjesiTheme
+import kotlinx.coroutines.launch
 import retrofit2.Call
 import retrofit2.Callback
 import retrofit2.Response
@@ -46,88 +52,106 @@ fun LoginPanelPreview(){
         SayfaGecisleri()
     }
 }
+@SuppressLint("UnusedMaterial3ScaffoldPaddingParameter")
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun LoginPanel(navController: NavController) {
+    //retrofit kısmı
     LaunchedEffect(key1 = true) {
         tumVeriler()
+        Log.e("deneme","123")
     }
 
-    Surface (color = colorResource(id = R.color.Tenrengi)){
+    val snackbarHostState = remember { SnackbarHostState() }
+    val scope = rememberCoroutineScope()
+    Scaffold (
+        snackbarHost = {
+            SnackbarHost(hostState = snackbarHostState)
+        },
 
-        val tfKullaniciAdi = remember {
-            mutableStateOf("")
-        }
-        val tfSifre = remember {
-            mutableStateOf("")
-        }
+        content ={
+            Surface (color = colorResource(id = R.color.Tenrengi)){
 
-        Column(
-            modifier = Modifier.fillMaxSize(),
-            verticalArrangement = Arrangement.SpaceEvenly,
-            horizontalAlignment = Alignment.CenterHorizontally
-        ) {
+                val tfKullaniciAdi = remember { mutableStateOf("") }
+                val tfSifre = remember { mutableStateOf("") }
 
-            //Logonun Olduğu Kısım
-            Image(
-                painter = painterResource(id = R.drawable.logo),
-                contentDescription = "", modifier = Modifier.size(200.dp))
+                Column(
+                    modifier = Modifier.fillMaxSize(),
+                    verticalArrangement = Arrangement.SpaceEvenly,
+                    horizontalAlignment = Alignment.CenterHorizontally
+                ) {
 
-            //Kullanıcı Adı Text Field Olduğu Kısım
-            Column {
-                val focusManager= LocalFocusManager.current
-                Text(text = "Kullanıcı Adı Giriniz: admin")
-                TextField(
-                    value = tfKullaniciAdi.value,
-                    onValueChange = { tfKullaniciAdi.value = it },
-                    label = { Text(text = "Kullanıcı Adı") },
-                    colors = TextFieldDefaults.textFieldColors(
-                        containerColor = colorResource(id = R.color.KahveRengi),
-                        cursorColor = colorResource(id = R.color.KoyuMavi)
-                    ),
-                    keyboardOptions = KeyboardOptions(imeAction = ImeAction.Next, keyboardType = KeyboardType.Text),
-                    keyboardActions = KeyboardActions(
-                        onNext = {focusManager.moveFocus(FocusDirection.Down)}
-                    )
-                )
-            }
+                    //Logonun Olduğu Kısım
+                    Image(
+                        painter = painterResource(id = R.drawable.logo),
+                        contentDescription = "", modifier = Modifier.size(200.dp))
 
-            //Şifre Text Field Olduğu Kısım
-            Column {
-                Text(text = "Şifre Giriniz: 0000")
-                TextField(
-                    value = tfSifre.value,
-                    onValueChange = { tfSifre.value = it },
-                    label = { Text(text = "Sifre") },
-                    colors = TextFieldDefaults.textFieldColors(
-                        containerColor = colorResource(id = R.color.KahveRengi)
-                    ),
-                    visualTransformation = PasswordVisualTransformation(),
-                    keyboardOptions = KeyboardOptions(imeAction = ImeAction.Done, keyboardType = KeyboardType.Number),
+                    //Kullanıcı Adı Text Field Olduğu Kısım
+                    Column {
+                        val focusManager= LocalFocusManager.current
+                        Text(text = "Kullanıcı Adı Giriniz: admin")
+                        TextField(
+                            value = tfKullaniciAdi.value,
+                            onValueChange = { tfKullaniciAdi.value = it },
+                            label = { Text(text = "Kullanıcı Adı") },
+                            colors = TextFieldDefaults.textFieldColors(
+                                containerColor = colorResource(id = R.color.KahveRengi),
+                                cursorColor = colorResource(id = R.color.KoyuMavi)
+                            ),
+                            keyboardOptions = KeyboardOptions(imeAction = ImeAction.Next, keyboardType = KeyboardType.Text),
+                            keyboardActions = KeyboardActions(
+                                onNext = {focusManager.moveFocus(FocusDirection.Down)}
+                            )
+                        )
+                    }
 
-                )
-            }
+                    //Şifre Text Field Olduğu Kısım
+                    Column {
+                        Text(text = "Şifre Giriniz: 0000")
+                        TextField(
+                            value = tfSifre.value,
+                            onValueChange = { tfSifre.value = it },
+                            label = { Text(text = "Sifre") },
+                            colors = TextFieldDefaults.textFieldColors(
+                                containerColor = colorResource(id = R.color.KahveRengi)
+                            ),
+                            visualTransformation = PasswordVisualTransformation(),
+                            keyboardOptions = KeyboardOptions(imeAction = ImeAction.Done, keyboardType = KeyboardType.Number),
 
-            //Giriş Butonu Olduğu Kısım
-            Button(onClick = { Log.e("Buton ","Giris yapildi")
-                if (tfKullaniciAdi.value=="admin"&&tfSifre.value=="0000"){
-                    navController.navigate("MainPanel")
+                            )
+                    }
+
+                    //Giriş Butonu Olduğu Kısım
+                    Button(onClick = { Log.e("Buton ","Giris yapildi")
+                        if (tfKullaniciAdi.value=="admin"&&tfSifre.value=="0000"){
+                            navController.navigate("MainPanel")
+                            scope.launch {
+                                snackbarHostState.showSnackbar(message = "Giris Yapildi")
+                            }
+                        }
+                        else{
+                            scope.launch {
+                                snackbarHostState.showSnackbar(message = "Bilgiler Eksik Ya Da Yanlis")
+                        }}
+
+                    },
+                        colors = ButtonDefaults.buttonColors(
+                            containerColor = colorResource(id = R.color.KoyuMavi),
+                            contentColor = Color.White
+                        ),
+                        modifier = Modifier.size(250.dp,50.dp)
+
+                    ) {
+                        Text(text = "Giris Yap")
+                    }
                 }
-
-                             },
-                colors = ButtonDefaults.buttonColors(
-                    containerColor = colorResource(id = R.color.KoyuMavi),
-                    contentColor = Color.White
-                ),
-                modifier = Modifier.size(250.dp,50.dp)
-
-            ) {
-                Text(text = "Giris Yap")
             }
         }
-    }
+    )
+
 }
 
+//retrofit kısmı
 fun tumVeriler(){
     val kisilerDaoInterface = ApiUtils.getVerilerDaoInterface()
 
