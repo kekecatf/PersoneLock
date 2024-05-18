a514f9cda4bf84625d40c958f455ed5f9b1f4a38
Atif Kekeç <kekecatf@outlook.com>
2024-05-18 11:41:00 +0300

    day12

--- app/src/main/java/com/example/muhendislikprojesi/LoginPanel.kt
+++ app/src/main/java/com/example/muhendislikprojesi/LoginPanel.kt
@@ -4,6 +4,7 @@ import android.annotation.SuppressLint
 
 import android.util.Log
 import androidx.compose.foundation.Image
+import androidx.compose.foundation.isSystemInDarkTheme
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
@@ -22,10 +23,12 @@ import androidx.compose.material3.CheckboxDefaults
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.SnackbarHost
 import androidx.compose.material3.SnackbarHostState
 import androidx.compose.material3.Surface
+import androidx.compose.material3.Switch
 import androidx.compose.material3.Text
 import androidx.compose.material3.TextField
 import androidx.compose.material3.TextFieldDefaults
@@ -70,118 +73,138 @@ import kotlin.coroutines.suspendCoroutine
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun LoginPanel(navController: NavController) {
+    //Tema İçin Gerekli Değişkenler
+    val isSystemDarkTheme = isSystemInDarkTheme()
+    var isDarkTheme by remember { mutableStateOf(isSystemDarkTheme) }
+    //Snackbar İçin Gerekli Değişkenler
     val snackbarHostState = remember { SnackbarHostState() }
     val scope = rememberCoroutineScope()
-    Scaffold(
-        snackbarHost = {
-            SnackbarHost(hostState = snackbarHostState)
-        },
-        content = {
-            Surface(color = colorResource(id = R.color.Tenrengi)) {
-                var userEmail by remember { mutableStateOf("") }
-                var userPassword by remember { mutableStateOf("") }
-                var passwordVisible by remember { mutableStateOf(false) }
-                var rememberMe by remember { mutableStateOf(false) }
+    //Veriler Değişkenleri
+    var userEmail by remember { mutableStateOf("") }
+    var userPassword by remember { mutableStateOf("") }
+    var passwordVisible by remember { mutableStateOf(false) }
+    //Enter İşlevi İçin Değişken
+    val focusManager = LocalFocusManager.current
 
-                val focusManager = LocalFocusManager.current
+    MuhendislikProjesiTheme(darkTheme = isDarkTheme){
+        Scaffold(
+            snackbarHost = {
+                SnackbarHost(hostState = snackbarHostState)
+            },
+            content = {
+                Surface(color = MaterialTheme.colorScheme.onPrimary) {
 
-                Column(
-                    modifier = Modifier.fillMaxSize(),
-                    verticalArrangement = Arrangement.SpaceEvenly,
-                    horizontalAlignment = Alignment.CenterHorizontally
-                ) {
-                    Image(
-                        painter = painterResource(id = R.drawable.logo2),
-                        contentDescription = "", modifier = Modifier.size(200.dp)
-                    )
-
-                    Column {
-                        TextField(
-                            value = userEmail,
-                            onValueChange = { userEmail = it },
-                            label = { Text(text = "Kullanıcı Adı") },
-                            colors = TextFieldDefaults.textFieldColors(
-                                containerColor = colorResource(id = R.color.KahveRengi),
-                                cursorColor = colorResource(id = R.color.KoyuMavi)
-                            ),
-                            keyboardOptions = KeyboardOptions(
-                                imeAction = ImeAction.Next,
-                                keyboardType = KeyboardType.Text
-                            ),
-                            keyboardActions = KeyboardActions(
-                                onNext = { focusManager.moveFocus(FocusDirection.Down) }
-                            )
+                    Column(
+                        modifier = Modifier.fillMaxSize(),
+                        verticalArrangement = Arrangement.SpaceEvenly,
+                        horizontalAlignment = Alignment.CenterHorizontally
+                    ) {
+                        //Logonun Olduğu Kısım
+                        Image(
+                            painter = painterResource(id = R.drawable.logo2),
+                            contentDescription = "", modifier = Modifier.size(200.dp)
                         )
-                    }
+                        //Tema Switchi Olduğu Kısım
+                        Switch(
+                            checked = isDarkTheme,
+                            onCheckedChange = {
+                                isDarkTheme = it
+                            }
+                        )
 
-                    Column {
-                        TextField(
-                            value = userPassword,
-                            onValueChange = { userPassword = it },
-                            label = { Text(text = "Sifre") },
-                            colors = TextFieldDefaults.textFieldColors(
-                                containerColor = colorResource(id = R.color.KahveRengi)
-                            ),
-                            visualTransformation = if (passwordVisible) VisualTransformation.None else PasswordVisualTransformation(),
-                            keyboardOptions = KeyboardOptions(
-                                imeAction = ImeAction.Done,
-                                keyboardType = KeyboardType.Number
-                            ),
-                            keyboardActions = KeyboardActions(
-                                //Enter'a basıldığında otomatil giriş yaptığı kısım
-                                onDone = {
-                                    focusManager.clearFocus()
-                                    CoroutineScope(Dispatchers.Main).launch {
-                                        val success = postVeri(userEmail, userPassword)
-                                        if (success) {
-                                            navController.navigate("MainPanel")
-                                        } else {
-                                            scope.launch {
-                                                snackbarHostState.showSnackbar(message = "Bilgiler Eksik Ya Da Yanlis")
+                        Column {
+                            //Kullanıcı Adı TextField Olduğu Kısım
+                            TextField(
+                                value = userEmail,
+                                onValueChange = { userEmail = it },
+                                label = { Text(text = "Kullanıcı Adı") },
+                                colors = TextFieldDefaults.textFieldColors(
+                                    containerColor = MaterialTheme.colorScheme.onPrimary,
+                                    cursorColor = MaterialTheme.colorScheme.onPrimary
+                                ),
+                                keyboardOptions = KeyboardOptions(
+                                    imeAction = ImeAction.Next,
+                                    keyboardType = KeyboardType.Text
+                                ),
+                                keyboardActions = KeyboardActions(
+                                    onNext = { focusManager.moveFocus(FocusDirection.Down) }
+                                )
+                            )
+                        }
+
+                        Column {
+                            //Şifre TextField Olduğu Kısım
+                            TextField(
+                                value = userPassword,
+                                onValueChange = { userPassword = it },
+                                label = { Text(text = "Sifre") },
+                                colors = TextFieldDefaults.textFieldColors(
+                                    containerColor = MaterialTheme.colorScheme.onPrimary,
+                                    cursorColor = MaterialTheme.colorScheme.onPrimary
+                                ),
+                                visualTransformation = if (passwordVisible) VisualTransformation.None else PasswordVisualTransformation(),
+                                keyboardOptions = KeyboardOptions(
+                                    imeAction = ImeAction.Done,
+                                    keyboardType = KeyboardType.Number
+                                ),
+                                keyboardActions = KeyboardActions(
+                                    //Enter'a basıldığında otomatil giriş yaptığı kısım
+                                    onDone = {
+                                        focusManager.clearFocus()
+                                        CoroutineScope(Dispatchers.Main).launch {
+                                            val success = postVeri(userEmail, userPassword)
+                                            if (success) {
+                                                navController.navigate("MainPanel")
+                                            } else {
+                                                scope.launch {
+                                                    snackbarHostState.showSnackbar(message = "Bilgiler Eksik Ya Da Yanlis")
+                                                }
                                             }
                                         }
                                     }
-                                }
-                            ),
-                            trailingIcon = {
-                                val image = if (passwordVisible)
-                                    painterResource(id = R.drawable.ic_visibility)
-                                else painterResource(id = R.drawable.ic_visibility_off)
+                                ),
+                                //Şifre Görünürlüğü Olduğu Kısım
+                                trailingIcon = {
+                                    val image = if (passwordVisible)
+                                        painterResource(id = R.drawable.ic_visibility)
+                                    else painterResource(id = R.drawable.ic_visibility_off)
 
-                                IconButton(onClick = {
-                                    passwordVisible = !passwordVisible
-                                }) {
-                                    Icon(painter = image, contentDescription = null)
-                                }
-                            }
-                        )
-                    }
-
-                    Button(
-                        onClick = {
-                            CoroutineScope(Dispatchers.Main).launch {
-                                val success = postVeri(userEmail, userPassword)
-                                if (success) {
-                                    navController.navigate("MainPanel")
-                                } else {
-                                    scope.launch {
-                                        snackbarHostState.showSnackbar(message = "Bilgiler Eksik Ya Da Yanlis")
+                                    IconButton(onClick = {
+                                        passwordVisible = !passwordVisible
+                                    }) {
+                                        Icon(painter = image, contentDescription = null)
                                     }
                                 }
-                            }
-                        },
-                        colors = ButtonDefaults.buttonColors(
-                            containerColor = colorResource(id = R.color.KoyuMavi),
-                            contentColor = Color.White
-                        ),
-                        modifier = Modifier.size(250.dp, 50.dp)
-                    ) {
-                        Text(text = "Giris Yap")
+                            )
+                        }
+                        //Giriş Butonu Olduğu Kısım
+                        Button(
+                            onClick = {
+                                CoroutineScope(Dispatchers.Main).launch {
+                                    val success = postVeri(userEmail, userPassword)
+                                    if (success) {
+                                        navController.navigate("MainPanel")
+                                    } else {
+                                        scope.launch {
+                                            snackbarHostState.showSnackbar(message = "Bilgiler Eksik Ya Da Yanlis")
+                                        }
+                                    }
+                                }
+                            },
+                            colors = ButtonDefaults.buttonColors(
+                                containerColor = MaterialTheme.colorScheme.onPrimary,
+                                contentColor = MaterialTheme.colorScheme.onPrimary
+                            ),
+                            modifier = Modifier.size(250.dp, 50.dp)
+                        ) {
+                            Text(text = "Giris Yap")
+                        }
                     }
                 }
             }
-        }
-    )
+        )
+    }
+
 }
 
 //RETROFİT KISMI
--- app/src/main/java/com/example/muhendislikprojesi/ui/theme/Theme.kt
+++ app/src/main/java/com/example/muhendislikprojesi/ui/theme/Theme.kt
@@ -1,5 +1,6 @@
 package com.example.muhendislikprojesi.ui.theme
 
+import android.annotation.SuppressLint
 import android.app.Activity
 import android.os.Build
 import androidx.compose.foundation.isSystemInDarkTheme
@@ -10,23 +11,42 @@ import androidx.compose.material3.dynamicLightColorScheme
 import androidx.compose.material3.lightColorScheme
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.SideEffect
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.toArgb
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.platform.LocalView
 import androidx.compose.ui.res.colorResource
+import androidx.core.content.ContextCompat
 import androidx.core.view.WindowCompat
 import com.example.muhendislikprojesi.R
 
-private val DarkColorScheme = darkColorScheme(
-    primary = Purple80,
-    secondary = PurpleGrey80,
-    tertiary = Pink80
+@Composable
+fun getColor(id: Int): Color {
+    return Color(ContextCompat.getColor(LocalContext.current, id))
+}
+
+@SuppressLint("ResourceAsColor")
+private val DarkColors = darkColorScheme(
+    primary = Color(R.color.KoyuMavi),
+    onPrimary = Color(R.color.AcikMavi),
+    primaryContainer = Color(R.color.AcikMavi),
+    onPrimaryContainer = Color(R.color.Tenrengi),
+    background = Color(R.color.Tenrengi),
+    onBackground = Color(R.color.KoyuMavi),
+    surface = Color(R.color.KoyuMavi),
+    onSurface = Color(R.color.AcikMavi)
 )
 
-private val LightColorScheme = lightColorScheme(
-    primary = Purple40,
-    secondary = PurpleGrey40,
-    tertiary = Pink40
+@SuppressLint("ResourceAsColor")
+private val  LightColors = lightColorScheme(
+    primary = Color(R.color.AcıkTenrengi),
+    onPrimary = Color(R.color.DahaKoyuTenrengi),
+    primaryContainer = Color(R.color.KoyuTenrengi),
+    onPrimaryContainer = Color(R.color.AcıkTenrengi),
+    background = Color(R.color.TuruncumsuTenrengi),
+    onBackground = Color(R.color.KoyuTenrengi),
+    surface = Color(R.color.KoyuTenrengi),
+    onSurface = Color(R.color.DahaKoyuTenrengi)
 
     /* Other default colors to override
     background = Color(0xFFFFFBFE),
@@ -42,7 +62,6 @@ private val LightColorScheme = lightColorScheme(
 @Composable
 fun MuhendislikProjesiTheme(
     darkTheme: Boolean = isSystemInDarkTheme(),
-    // Dynamic color is available on Android 12+
     dynamicColor: Boolean = true,
     content: @Composable () -> Unit
 ) {
@@ -51,16 +70,16 @@ fun MuhendislikProjesiTheme(
             val context = LocalContext.current
             if (darkTheme) dynamicDarkColorScheme(context) else dynamicLightColorScheme(context)
         }
-
-        darkTheme -> DarkColorScheme
-        else -> LightColorScheme
+        darkTheme -> DarkColors
+        else -> LightColors
     }
+
     val view = LocalView.current
-    val statusBarRengi = colorResource(id = R.color.Gri).toArgb()
+    val statusBarColor = colorResource(id = R.color.Gri).toArgb()
     if (!view.isInEditMode) {
         SideEffect {
             val window = (view.context as Activity).window
-            window.statusBarColor = statusBarRengi
+            window.statusBarColor = statusBarColor
             WindowCompat.getInsetsController(window, view).isAppearanceLightStatusBars = darkTheme
         }
     }
